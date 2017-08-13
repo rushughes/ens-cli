69c26a90cae88703c35a77cbf29fd2b22d3a7efd
Rus Hughes <russell.hughes@gmail.com>
2017-08-13 12:01:12 +0700

    we can now compile and migrate the contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,20 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+/* global artifacts:true */
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+const ENS = artifacts.require('./ENS.sol')
+const Registrar = artifacts.require('./Registrar.sol')
+const Deed = artifacts.require('./Deed.sol')
+const PublicResolver = artifacts.require('./PublicResolver.sol')
+
+module.exports = (deployer) => {
+  // The hex 0x93c... string reolves to the TLD 'eth' via namehash('eth')
+  // So, this makes ENS responsible for the TLD 'eth'
+  deployer.deploy(ENS,
+                  '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae')
+    .then(() => {
+      // This transfers ownership of the TLD 'eth' to the Registrar contract
+      deployer.deploy(Registrar, ENS.deployed().address,
+                      '0x93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae')
+    })
+  deployer.deploy(Deed)
+  deployer.deploy(PublicResolver)
+}
